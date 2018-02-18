c02ba177ba8f78bfd2c2820e8968d7ef3de8efc7
Joe Jobes <jmrjobes@gmail.com>
2018-02-18 15:15:33 -0700

    Add show action to conditions controller and set_category

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,7 +1,17 @@
 class ConditionsController < ApplicationController
+  before_action :set_condition, only: [:show]
 
   def index
     @conditions = Condition.all
   end
 
-end 
+  def show
+  end
+
+  private
+
+    def set_condition
+      @category = Condition.find(params[:id])
+    end
+
+end
