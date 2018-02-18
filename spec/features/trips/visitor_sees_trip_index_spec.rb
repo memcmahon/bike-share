4a7a7e636b825ed96a46cb9934a5b31d5c560fcc
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-18 15:34:15 -0700

    Updates first visitor trip index spec

--- spec/features/trips/visitor_sees_trip_index_spec.rb
+++ spec/features/trips/visitor_sees_trip_index_spec.rb
@@ -7,7 +7,37 @@ describe "As a visitor" do
 
       visit trips_path
 
-      binding.pry
+      expect(page).to have_content(trip[0].duration)
+      expect(page).to have_content(trip[1].duration)
+      expect(page).to have_content(trip[2].duration)
+      expect(page).to have_content(trip[3].duration)
+      expect(page).to have_content(trip[4].duration)
+      expect(page).to have_content(trip[5].duration)
+      expect(page).to have_content(trip[6].duration)
+      expect(page).to have_content(trip[7].duration)
+      expect(page).to have_content(trip[8].duration)
+      expect(page).to have_content(trip[9].duration)
+      expect(page).to have_content(trip[10].duration)
+      expect(page).to have_content(trip[11].duration)
+      expect(page).to have_content(trip[12].duration)
+      expect(page).to have_content(trip[13].duration)
+      expect(page).to have_content(trip[14].duration)
+      expect(page).to have_content(trip[15].duration)
+      expect(page).to have_content(trip[16].duration)
+      expect(page).to have_content(trip[17].duration)
+      expect(page).to have_content(trip[18].duration)
+      expect(page).to have_content(trip[19].duration)
+      expect(page).to have_content(trip[20].duration)
+      expect(page).to have_content(trip[21].duration)
+      expect(page).to have_content(trip[22].duration)
+      expect(page).to have_content(trip[23].duration)
+      expect(page).to have_content(trip[24].duration)
+      expect(page).to have_content(trip[25].duration)
+      expect(page).to have_content(trip[26].duration)
+      expect(page).to have_content(trip[27].duration)
+      expect(page).to have_content(trip[28].duration)
+      expect(page).to have_content(trip[29].duration)
+      expect(page).to_not have_content(trip[31].duration)
     end
   end
 end
