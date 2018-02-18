ca083d24781af6f0ef3dd10373f2aab24be785d1
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-17 21:57:54 -0700

    Adds log in form

--- spec/features/authentication/registered_user_can_log_in_spec.rb
+++ spec/features/authentication/registered_user_can_log_in_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "As a registered user" do
   describe "they visit root path" do
-    describe "they can log in" do
+    it "they can log in" do
       user = User.create(first_name: "Demo", last_name: "User", email: "demo@fakemail.com", password: "Test")
 
       visit root_path
@@ -11,13 +11,13 @@ describe "As a registered user" do
 
       fill_in("email", with: "demo@fakemail.com")
       fill_in("password", with: "Test")
-      click_on("Log In")
+      click_on("LogIn")
 
       expect(current_path).to eq('/dashboard')
       within ("navbar")
         expect(page).to have_content("Logged in as demo@fakemail.com")
-      expect(page).to have_content("Log Out")
-      expect(page).to_not have_content("Log In")
+        expect(page).to have_content("Log Out")
+        expect(page).to_not have_content("Log In")
     end
   end
 end
