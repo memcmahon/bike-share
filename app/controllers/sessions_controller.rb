417f8e70457c0da002eabab302cf86522f74b4be
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-17 22:00:39 -0700

    Adds create action for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in as #{current_user.email}."
+      redirect_to '/dashboard'
+    else
+      render :new
+    end
+  end
 end
