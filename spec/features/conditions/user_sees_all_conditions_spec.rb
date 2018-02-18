544252b7ba1ce8b63a5a103eacf78f91b47ec32a
Joe Jobes <jmrjobes@gmail.com>
2018-02-18 14:40:27 -0700

    Add more asssertions to conditions index spec

--- spec/features/conditions/user_sees_all_conditions_spec.rb
+++ spec/features/conditions/user_sees_all_conditions_spec.rb
@@ -5,19 +5,27 @@ describe "user sees all conditions" do
     it "shows all conditions" do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      condition = create(:condition)
+      condition = create_list(:condition, 2)
 
       visit root_path
       click_on "Conditions"
 
       expect(current_path).to eq(conditions_path)
-      expect(page).to have_content(condition.max_temp_f)
-      expect(page).to have_content(condition.mean_temp_f)
-      expect(page).to have_content(condition.min_temp_f)
-      expect(page).to have_content(condition.mean_visibility_miles)
-      expect(page).to have_content(condition.mean_wind_speed_mph)
-      expect(page).to have_content(condition.precipitation_inches)
-      expect(page).to have_content(condition.date)
+      expect(page).to have_content(condition[0].max_temp_f)
+      expect(page).to have_content(condition[0].mean_temp_f)
+      expect(page).to have_content(condition[0].min_temp_f)
+      expect(page).to have_content(condition[0].mean_visibility_miles)
+      expect(page).to have_content(condition[0].mean_wind_speed_mph)
+      expect(page).to have_content(condition[0].precipitation_inches)
+      expect(page).to have_content(condition[0].date)
+
+      expect(page).to have_content(condition[1].max_temp_f)
+      expect(page).to have_content(condition[1].mean_temp_f)
+      expect(page).to have_content(condition[1].min_temp_f)
+      expect(page).to have_content(condition[1].mean_visibility_miles)
+      expect(page).to have_content(condition[1].mean_wind_speed_mph)
+      expect(page).to have_content(condition[1].precipitation_inches)
+      expect(page).to have_content(condition[1].date)
     end
   end
 end
