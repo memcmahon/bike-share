8c401aa4859de507c56573c8056ac6d3350ae6e7
Joe Jobes <jmrjobes@gmail.com>
2018-02-18 15:12:01 -0700

    Add feature for visitors seeing conditions index and show

---
+++ spec/features/conditions/visitor_sees_all_conditions_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "visitor sees all conditions" do
+  context "link from the root path" do
+    it "shows all conditions" do
+      visitor = create(:user)
+      condition = create_list(:condition, 2)
+
+      visit root_path
+      click_on "Conditions"
+
+      expect(current_path).to eq(conditions_path)
+      expect(page).to have_content(condition[0].max_temp_f)
+      expect(page).to have_content(condition[0].mean_temp_f)
+      expect(page).to have_content(condition[0].min_temp_f)
+      expect(page).to have_content(condition[0].mean_visibility_miles)
+      expect(page).to have_content(condition[0].mean_wind_speed_mph)
+      expect(page).to have_content(condition[0].precipitation_inches)
+      expect(page).to have_content(condition[0].date)
+
+      expect(page).to have_content(condition[1].max_temp_f)
+      expect(page).to have_content(condition[1].mean_temp_f)
+      expect(page).to have_content(condition[1].min_temp_f)
+      expect(page).to have_content(condition[1].mean_visibility_miles)
+      expect(page).to have_content(condition[1].mean_wind_speed_mph)
+      expect(page).to have_content(condition[1].precipitation_inches)
+      expect(page).to have_content(condition[1].date)
+    end
+  end
+end
---
+++ spec/features/conditions/visitor_sees_single_condition_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "visitor sees single condition" do
+  context "link from conditions index page" do
+    it "shows all attributes for condition" do
+      condition = create(:condition)
+      visit conditions_path
+
+      find(".condition-1").click_on
+
+      expect(current_path).to eq(condition_path(condition))
+      expect(page).to have_content(condition.max_temp_f)
+      expect(page).to have_content(condition.mean_temp_f)
+      expect(page).to have_content(condition.min_temp_f)
+      expect(page).to have_content(condition.max_dew_point_f)
+      expect(page).to have_content(condition.mean_dew_point_f)
+      expect(page).to have_content(condition.max_humidity)
+      expect(page).to have_content(condition.mean_humidity)
+      expect(page).to have_content(condition.min_humidity)
+      expect(page).to have_content(condition.max_sea_level_pressure_inches)
+      expect(page).to have_content(condition.mean_sea_level_pressure_inches)
+      expect(page).to have_content(condition.min_sea_level_pressure_inches)
+      expect(page).to have_content(condition.max_visibility_miles)
+      expect(page).to have_content(condition.mean_visibility_miles)
+      expect(page).to have_content(condition.min_visibility_miles)
+      expect(page).to have_content(condition.max_wind_speed_mph)
+      expect(page).to have_content(condition.mean_wind_speed_mph)
+      expect(page).to have_content(condition.max_gust_speed_mph)
+      expect(page).to have_content(condition.precipitation_inches)
+      expect(page).to have_content(condition.cloud_cover)
+      expect(page).to have_content(condition.events)
+      expect(page).to have_content(condition.wind_dir_degrees)
+      expect(page).to have_content(condition.zip_code)
+      expect(page).to have_content(condition.date)
+    end
+  end
+end
