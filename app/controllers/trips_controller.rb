bc19a1c6ddfcd97afd155ebfe2bab9953526f25f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-18 15:11:41 -0700

    Adds filter to set trip on trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,11 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show]
+
   def show
   end
+
+  private
+    def set_trip
+      @trip = Trip.find(params[:id])
+    end
 end
