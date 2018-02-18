1e65e52c01fb41799982091a6ea7227d0b402d8f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-17 18:53:57 -0700

    Adds create action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new()
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "logged in as #{current_user.email}"
+      redirect_to :show
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
 end
