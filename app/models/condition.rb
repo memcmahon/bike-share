d7be9a97a57eabbd590def4dd543fab39f6508a7
Joe Jobes <jmrjobes@gmail.com>
2018-02-18 11:30:30 -0700

    Add condition model

---
+++ app/models/condition.rb
@@ -0,0 +1,11 @@
+class Condition < ApplicationRecord
+  has_many :trips
+  validates_presence_of :date,
+                        :max_temp_f,
+                        :mean_temp_f,
+                        :min_temp_f,
+                        :mean_humidity,
+                        :mean_visibility_miles,
+                        :mean_wind_speed_mph,
+                        :precipitation_inches
+end
