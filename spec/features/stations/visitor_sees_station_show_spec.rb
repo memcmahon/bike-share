131e824d4a5602576a7e58edb4f803cbe9ea33f0
Luis Garcia <garcia@yahoo.com>
2018-02-19 02:06:14 -0700

    resolve merge conflicts

--- spec/features/stations/visitor_sees_station_show_spec.rb
+++ spec/features/stations/visitor_sees_station_show_spec.rb
@@ -7,9 +7,9 @@ describe "As a visitor" do
 
       visit station_path(station.slug)
       
-      expect(current_path).to eq("/stations/85-street")
-      expect(page).to have_content("Name: 85 Street")
-      expect(page).to have_content("Dock Count: 85")
+      expect(current_path).to eq("/stations/1-street")
+      expect(page).to have_content("Name: 1 Street")
+      expect(page).to have_content("Dock Count: 1")
       expect(page).to have_content("Lat: 2")
       expect(page).to have_content("Long: 3")
       expect(page).to have_content("City: San Francisco")
