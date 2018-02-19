41de018ef2c36cf677f643bec020a56db178c61f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-19 11:25:30 -0700

    Removes validations for trip attributes that may be missing in csv

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,6 +8,5 @@ class Trip < ApplicationRecord
                         :end_station_name,
                         :end_station_id,
                         :bike_id,
-                        :subscription_type,
-                        :zip_code
+                        :subscription_type
 end
