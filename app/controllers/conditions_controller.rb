533548739e4915417f65430467993596c0c4e33d
Joe Jobes <jmrjobes@gmail.com>
2018-02-19 09:27:38 -0700

    Add paginate to controller index'

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -2,7 +2,7 @@ class ConditionsController < ApplicationController
   before_action :set_condition, only: [:show]
 
   def index
-    @conditions = Condition.all
+    @conditions = Condition.all.paginate(page: params[:page], per_page: 30)
   end
 
   def show
