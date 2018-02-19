a45d319cd58f5afa53115a99d7577b4626a42205
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-19 13:46:34 -0700

    Adds shortest and longest ride calculation on model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :condition
+
   validates_presence_of :duration,
                         :start_date,
                         :start_station_name,
@@ -9,6 +10,8 @@ class Trip < ApplicationRecord
                         :end_station_id,
                         :bike_id,
                         :subscription_type
+
+  scope :sort_by_duration, -> { order(duration: :asc) }
 
   def self.average_duration
     average(:duration)
