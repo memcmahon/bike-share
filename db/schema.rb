f8a588c35d70c8f3e119987d54dacb5559e39e79
Luis Garcia <garcia@yahoo.com>
2018-02-18 23:12:47 -0700

    Add slug migration and passing station show test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218004003) do
+ActiveRecord::Schema.define(version: 20180219060342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20180218004003) do
     t.integer "dock_count"
     t.string "city"
     t.date "installation_date"
+    t.string "slug"
   end
 
   create_table "statuses", force: :cascade do |t|
