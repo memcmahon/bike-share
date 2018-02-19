d50214b230ea32dae244459baf7813f4d979f7ca
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-19 13:25:47 -0700

    Adds authorization check to tripsdashboard#index

--- app/controllers/trips_dashboard_controller.rb
+++ app/controllers/trips_dashboard_controller.rb
@@ -1,5 +1,10 @@
 class TripsDashboardController < ApplicationController
   def index
-    @trips = Trip.all
+    if current_user
+      @trips = Trip.all
+    else
+      flash[:notice] = "Please register or log in to visit this page"
+      redirect_to root_path
+    end
   end
 end
