35b8218b55cc062953879bed9580ed09e38ffdda
Luis Garcia <garcia@yahoo.com>
2018-02-18 23:52:06 -0700

    modify visitor station show test

--- spec/features/stations/visitor_sees_station_show_spec.rb
+++ spec/features/stations/visitor_sees_station_show_spec.rb
@@ -7,13 +7,13 @@ describe "As a visitor" do
 
       visit station_path(station.slug)
 
-      expect(current_path).to eq("/stations/1-street  ")
-      expect(page).to have_content("1 Street")
-      expect(page).to have_content("1")
-      expect(page).to have_content("2")
-      expect(page).to have_content("3")
-      expect(page).to have_content("San Francisco")
-      expect(page).to have_content("1991-12-12")    
+      expect(current_path).to eq("/stations/85-street")
+      expect(page).to have_content("Name: 85 Street")
+      expect(page).to have_content("Dock Count: 85")
+      expect(page).to have_content("Lat: 2")
+      expect(page).to have_content("Long: 3")
+      expect(page).to have_content("City: San Francisco")
+      expect(page).to have_content("Installation Date: 1991-12-12")    
     end
   end
 end
