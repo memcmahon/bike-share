e404d461426af83527fbe703e6eeaa0981376cc9
Joe Jobes <jmrjobes@gmail.com>
2018-02-20 15:39:25 -0700

    Update model tests with new methods

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -20,58 +20,53 @@ describe Condition, type: :model do
   describe 'class methods' do
     before(:each) do
       @condition1 = create(:condition, max_temp_f: 55, date: '2/2/1993', precipitation_inches: 0.2, mean_wind_speed_mph: 23, mean_visibility_miles: 10)
-      @condition2 = create(:condition, max_temp_f: 51, date: '2/2/1993', precipitation_inches: 0.2, mean_wind_speed_mph: 23, mean_visibility_miles: 10)
+      @condition2 = create(:condition, max_temp_f: 51, date: '2/4/1993', precipitation_inches: 0.2, mean_wind_speed_mph: 23, mean_visibility_miles: 10)
       @condition3 = create(:condition, max_temp_f: 58, date: '2/3/1993', precipitation_inches: 0.2, mean_wind_speed_mph: 23, mean_visibility_miles: 10)
-      @condition4 = create(:condition, max_temp_f: 60, date: '2/3/1993', precipitation_inches: 0.2, mean_wind_speed_mph: 23, mean_visibility_miles: 10)
-      @condition5 = create(:condition, max_temp_f: 30, date: '10/2/1993', precipitation_inches: 1.2, mean_wind_speed_mph: 33, mean_visibility_miles: 19)
+      @condition4 = create(:condition, max_temp_f: 60, date: '2/5/1993', precipitation_inches: 0.2, mean_wind_speed_mph: 23, mean_visibility_miles: 10)
       create_list(:trip, 5, condition_id: @condition1.id)
       create_list(:trip, 12, condition_id: @condition2.id)
       create_list(:trip, 13, condition_id: @condition3.id)
       create_list(:trip, 20, condition_id: @condition4.id)
     end
     it "returns conditions joined on max temp range" do
-      expect(Condition.trips_with_max_temp(50..60).count).to eq(50)
-      expect(Condition.trips_with_max_temp(50..60)).to_not include(@condition5)
+      expect(Condition.trips_with_max_temp(50..60)).to eq([20, 13, 12, 5])
     end
     it "returns average trips per day within max temp range" do
       expect(Condition.avg_trips_max_temp(50..60)).to eq(12.0)
     end
     it "returns count of most and least trips within max temp range" do
-      expect(Condition.extremum_trips_max_temp(50..60, 'DESC')).to eq(33)
-      expect(Condition.extremum_trips_max_temp(50..60, 'ASC')).to eq(17)
+      expect(Condition.extremum_trips_max_temp('max', 50..60)).to eq(20)
+      expect(Condition.extremum_trips_max_temp('min', 50..60)).to eq(5)
     end
     it "returns conditions joined on precipitation range" do
-      expect(Condition.trips_with_precipitation(0..0.5).count).to eq(50)
-      expect(Condition.trips_with_precipitation(0..0.5)).to_not include(@condition5)
+      expect(Condition.trips_with_precipitation(0..0.5)).to eq([20, 13, 12, 5])
     end
     it "returns average count of trips within precipitation range" do
       expect(Condition.avg_trips_precipitation(0..0.5)).to eq(12)
     end
     it "returns count of most and least trips within precipitation range" do
-      expect(Condition.extremum_trips_precipitation(0..0.5, 'DESC')).to eq(33)
-      expect(Condition.extremum_trips_precipitation(0..0.5, 'ASC')).to eq(17)
+      expect(Condition.extremum_trips_precipitation('max', 0..0.5)).to eq(20)
+      expect(Condition.extremum_trips_precipitation('min', 0..0.5)).to eq(5)
     end
     it "returns conditions joined on wind speed range" do
-      expect(Condition.trips_wind_speed(20..25).count).to eq(50)
-      expect(Condition.trips_wind_speed(20..25)).to_not include(@condition5)
+      expect(Condition.trips_wind_speed(20..25)).to eq([20, 13, 12, 5])
     end
     it "returns average count of trips within wind speed range" do
       expect(Condition.avg_trips_wind_speed(20..25)).to eq(12)
     end
     it "returns count of most and least trips within wind speed range" do
-      expect(Condition.extremum_trips_wind_speed(20..25, 'DESC')).to eq(33)
-      expect(Condition.extremum_trips_wind_speed(20..25, 'ASC')).to eq(17)
+      expect(Condition.extremum_trips_wind_speed('max', 20..25)).to eq(20)
+      expect(Condition.extremum_trips_wind_speed('min', 20..25)).to eq(5)
     end
     it "returns conditions joined on visibility range" do
-      expect(Condition.trips_visibility(5..10).count).to eq(50)
-      expect(Condition.trips_visibility(5..10)).to_not include(@condition5)
+      expect(Condition.trips_visibility(5..10)).to eq([20, 13, 12, 5])
     end
     it "returns average count of trips within visibility range" do
       expect(Condition.avg_trips_visibility(5..10)).to eq(12)
     end
     it "returns count of most and least trips within visibility range" do
-      expect(Condition.extremum_trips_visibility(5..10, 'DESC')).to eq(33)
-      expect(Condition.extremum_trips_visibility(5..10, 'ASC')).to eq(17)
+      expect(Condition.extremum_trips_visibility('max', 5..10)).to eq(20)
+      expect(Condition.extremum_trips_visibility('min', 5..10)).to eq(5)
     end
   end
 end
