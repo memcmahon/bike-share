b4ff01fa7ba79400854f9cbcea2dfe95c6c31723
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-20 16:35:38 -0700

    Adds spec for user dashboard

--- spec/features/authentication/admin_user_can_log_in_spec.rb
+++ spec/features/authentication/admin_user_can_log_in_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe "As an admin" do
+  before(:each) do
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
+
   describe "they visit the login page" do
-    before(:each) do
-      @admin = create(:admin)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-    end
     it "they can log in" do
       visit login_path
 
@@ -14,6 +15,23 @@ describe "As an admin" do
       click_on("LogIn")
 
       expect(current_path).to eq('/admin/dashboard')
+    end
+  end
+
+  describe "they visit admin/dashboard" do
+    it "they see their user profile" do
+      visit admin_dashboard_path
+
+      expect(page).to have_content(@admin.first_name)
+      expect(page).to have_content(@admin.last_name)
+      expect(page).to have_content(@admin.email)
+    end
+
+    it "they see a log out link, but not a log in link" do
+      visit admin_dashboard_path
+
+      expect(page).to have_content("Log Out")
+      expect(page).to_not have_content("Log In")
     end
   end
 end
