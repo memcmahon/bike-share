164b2ea1c9ddce9fe3874d5fe7bda188e26496b3
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-20 18:25:57 -0700

    Adds spec for admin deletes trips

---
+++ spec/features/trips/admin_can_delete_a_trip_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe "As an admin" do
+  before(:each) do
+    @trips = create_list(:trip, 5)
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
+
+  describe "They visit trip dashboard" do
+    it "they can delete a trip" do
+      visit trips_path
+      expect(Trip.count).to eq(5)
+
+      click_on("Delete", match: :first)
+
+      expect(Trip.count).to eq(4)
+      expect(current_path).to eq(trips_path)
+      expect(page).to_not have_content(@trips[0].duration)
+    end
+  end
+
+  describe "They visit trip show" do
+    it "they can delete a trip" do
+      visit trip_path(@trips[0])
+
+      click_on("Delete")
+
+      expect(current_path).to eq(trips_path)
+      expect(page).to_not have_content(@trips[0].duration)
+    end
+  end
+end
