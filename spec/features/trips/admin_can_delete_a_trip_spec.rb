95190a67017bd3982de4c8737e3ca77633ea01c5
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-20 19:37:42 -0700

    Adds feature spec for admin deletes trips

--- spec/features/trips/admin_can_delete_a_trip_spec.rb
+++ spec/features/trips/admin_can_delete_a_trip_spec.rb
@@ -9,17 +9,24 @@ describe "As an admin" do
 
   describe "They visit trip dashboard" do
     it "they can delete a trip" do
+      visit login_path
+
+      fill_in("email", with: @admin.email)
+      fill_in("password", with: @admin.password)
+
       visit trips_path
 
       click_on("Delete", match: :first)
-
-      expect(current_path).to eq(trips_path)
-      expect(page).to_not have_content(@trips[0].duration)
     end
   end
 
   describe "They visit trip show" do
     it "they can delete a trip" do
+      visit login_path
+
+      fill_in("email", with: @admin.email)
+      fill_in("password", with: @admin.password)
+
       visit trip_path(@trips[0])
 
       click_on("Delete")
