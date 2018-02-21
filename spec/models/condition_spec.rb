f3c04a73b6f5dba890c46800a0551434c47baf73
Joe Jobes <jmrjobes@gmail.com>
2018-02-21 16:57:56 -0700

    Update condition spec with new model tests

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -29,45 +29,27 @@ describe Condition, type: :model do
       create_list(:trip, 13, start_station: @station_1, end_station: @station_1, condition_id: @condition3.id)
       create_list(:trip, 20, start_station: @station_1, end_station: @station_1, condition_id: @condition4.id)
     end
-    it "returns conditions joined on max temp range" do
-      expect(Condition.trips_with_max_temp(50..60)).to eq([20, 13, 12, 5])
+    it "returns count of trips per day within a range" do
+      expect(Condition.trip_counts({max_temp_f: 50..60})).to eq([20, 13, 12, 5])
+      expect(Condition.trip_counts({mean_wind_speed_mph: 20..24})).to eq([20, 13, 12, 5])
+      expect(Condition.trip_counts({precipitation_inches: 0..0.5})).to eq([20, 13, 12, 5])
+      expect(Condition.trip_counts({mean_visibility_miles: 8..12})).to eq([20, 13, 12, 5])
     end
-    it "returns average trips per day within max temp range" do
-      expect(Condition.avg_trips_max_temp(50..60)).to eq(12.0)
+    it "returns average trips per day within  range" do
+      expect(Condition.avg_trips({max_temp_f: 50..60})).to eq(12.0)
+      expect(Condition.avg_trips({precipitation_inches: 0..0.5})).to eq(12)
+      expect(Condition.avg_trips({mean_wind_speed_mph: 20..24})).to eq(12)
+      expect(Condition.avg_trips({mean_visibility_miles: 8..12})).to eq(12)
     end
     it "returns count of most and least trips within max temp range" do
-      expect(Condition.extremum_trips_max_temp('max', 50..60)).to eq(20)
-      expect(Condition.extremum_trips_max_temp('min', 50..60)).to eq(5)
-    end
-    it "returns conditions joined on precipitation range" do
-      expect(Condition.trips_with_precipitation(0..0.5)).to eq([20, 13, 12, 5])
-    end
-    it "returns average count of trips within precipitation range" do
-      expect(Condition.avg_trips_precipitation(0..0.5)).to eq(12)
-    end
-    it "returns count of most and least trips within precipitation range" do
-      expect(Condition.extremum_trips_precipitation('max', 0..0.5)).to eq(20)
-      expect(Condition.extremum_trips_precipitation('min', 0..0.5)).to eq(5)
-    end
-    it "returns conditions joined on wind speed range" do
-      expect(Condition.trips_wind_speed(20..25)).to eq([20, 13, 12, 5])
-    end
-    it "returns average count of trips within wind speed range" do
-      expect(Condition.avg_trips_wind_speed(20..25)).to eq(12)
-    end
-    it "returns count of most and least trips within wind speed range" do
-      expect(Condition.extremum_trips_wind_speed('max', 20..25)).to eq(20)
-      expect(Condition.extremum_trips_wind_speed('min', 20..25)).to eq(5)
-    end
-    it "returns conditions joined on visibility range" do
-      expect(Condition.trips_visibility(5..10)).to eq([20, 13, 12, 5])
-    end
-    it "returns average count of trips within visibility range" do
-      expect(Condition.avg_trips_visibility(5..10)).to eq(12)
-    end
-    it "returns count of most and least trips within visibility range" do
-      expect(Condition.extremum_trips_visibility('max', 5..10)).to eq(20)
-      expect(Condition.extremum_trips_visibility('min', 5..10)).to eq(5)
+      expect(Condition.trips_extremums('max', {max_temp_f: 50..60})).to eq(20)
+      expect(Condition.trips_extremums('min', {max_temp_f: 50..60})).to eq(5)
+      expect(Condition.trips_extremums('max', {mean_wind_speed_mph: 20..24})).to eq(20)
+      expect(Condition.trips_extremums('min', {mean_wind_speed_mph: 20..24})).to eq(5)
+      expect(Condition.trips_extremums('max', {mean_visibility_miles: 8..12})).to eq(20)
+      expect(Condition.trips_extremums('min', {mean_visibility_miles: 8..12})).to eq(5)
+      expect(Condition.trips_extremums('max', {precipitation_inches: 0..0.5})).to eq(20)
+      expect(Condition.trips_extremums('min', {precipitation_inches: 0..0.5})).to eq(5)
     end
   end
 end
