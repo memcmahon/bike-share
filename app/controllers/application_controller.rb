1283daac472bc1d168937f14238cd6d731ba2714
Joe Jobes <jmrjobes@gmail.com>
2018-02-21 16:02:11 -0700

    Add helper method for printing range in string form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :print_range
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def print_range(range)
+    range.to_s.split('..').join(' - ')
+  end
 end
-  
