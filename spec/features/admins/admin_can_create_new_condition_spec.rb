b11d2c417ee6574de84891a2487247f2977c84b6
Joe Jobes <jmrjobes@gmail.com>
2018-02-21 07:52:17 -0700

    Delete extraneous data

--- spec/features/admins/admin_can_create_new_condition_spec.rb
+++ spec/features/admins/admin_can_create_new_condition_spec.rb
@@ -12,25 +12,10 @@ describe "as an admin" do
       fill_in "condition[max_temp_f]", with: 12.2
       fill_in "condition[mean_temp_f]", with: 12.2
       fill_in "condition[min_temp_f]", with: 12.2
-      fill_in "condition[max_dew_point_f]", with: 12.2
-      fill_in "condition[mean_dew_point_f]", with: 12.2
-      fill_in "condition[min_dew_point_f]", with: 12.2
-      fill_in "condition[max_humidity]", with: 12.2
       fill_in "condition[mean_humidity]", with: 12.2
-      fill_in "condition[min_humidity]", with: 12.2
-      fill_in "condition[max_sea_level_pressure_inches]", with: 12.2
-      fill_in "condition[mean_sea_level_pressure_inches]", with: 12.2
-      fill_in "condition[min_sea_level_pressure_inches]", with: 12.2
-      fill_in "condition[max_visibility_miles]", with: 12.2
       fill_in "condition[mean_visibility_miles]", with: 12.2
-      fill_in "condition[min_visibility_miles]", with: 12.2
-      fill_in "condition[max_wind_Speed_mph]", with: 12.2
       fill_in "condition[mean_wind_speed_mph]", with: 12.2
-      fill_in "condition[max_gust_speed_mph]", with: 12.2
       fill_in "condition[precipitation_inches]", with: 12.2
-      fill_in "condition[cloud_cover]", with: 12.2
-      fill_in "condition[events]", with: 'Fog'
-      fill_in "condition[wind_dir_degrees]", with: 12.2
       fill_in "condition[zip_code]", with: 122323
 
       click_on "Create Condition"
@@ -39,24 +24,10 @@ describe "as an admin" do
       expect(page).to have_content("Max Temperature: 12.2")
       expect(page).to have_content("Mean Temperature: 12.2")
       expect(page).to have_content("Minimum Temperature: 12.2")
-      expect(page).to have_content("Max Dew Point: 12.2")
-      expect(page).to have_content("Mean Dew Point: 12.2")
-      expect(page).to have_content("Max Humidity: 12.2")
       expect(page).to have_content("Mean Humidity: 12.2")
-      expect(page).to have_content("Minimum Humidity: 12.2")
-      expect(page).to have_content("Max Sea Level Pressure: 12.2")
-      expect(page).to have_content("Mean Sea Level Pressure: 12.2")
-      expect(page).to have_content("Minimum Sea Level Pressure: 12.2")
-      expect(page).to have_content("Max Visibility Levels: 12.2")
       expect(page).to have_content("Mean Visibility Levels: 12.2")
-      expect(page).to have_content("Minimum Visibility Levels: 12.2")
-      expect(page).to have_content("Max Wind Speed: 12.2")
       expect(page).to have_content("Mean Wind Speed: 12.2")
-      expect(page).to have_content("Max Gust Speed: 12.2")
       expect(page).to have_content("Precipitation Levels: 12.2")
-      expect(page).to have_content("Cloud Coverage: 12.2")
-      expect(page).to have_content("Possible Events: Fog")
-      expect(page).to have_content("Wind Direction: 12.2")
       expect(page).to have_content("Zipcode: 122323")
     end
   end
