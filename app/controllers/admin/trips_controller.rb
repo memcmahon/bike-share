6f37923148a7acda5d1f791747c32a778ad04371
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-20 20:57:32 -0700

    Adds edit action to trips controller

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,6 +1,20 @@
 class Admin::TripsController < Admin::BaseController
+  before_action :set_trip, only: [:edit, :update]
+
   def destroy
     Trip.destroy(params[:id])
     redirect_to trips_path
   end
+
+  def edit
+  end
+
+  private
+    def set_trip
+      Trip.find(params[:id])
+    end
+
+    def trip_params
+      params.require(:trip).permit(:id)
+    end
 end
