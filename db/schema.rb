89edfc6f9d39459309546cd43c6949ad04c47acb
Joe Jobes <jmrjobes@gmail.com>
2018-02-21 18:16:07 -0700

    Ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221224004) do
+ActiveRecord::Schema.define(version: 20180222011136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 20180221224004) do
     t.string "name"
     t.text "description"
     t.integer "status"
-  end
-
-  create_table "carts", force: :cascade do |t|
-    t.integer "quantity"
-    t.bigint "accessory_id"
-    t.bigint "user_id"
-    t.index ["accessory_id"], name: "index_carts_on_accessory_id"
-    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "conditions", force: :cascade do |t|
@@ -57,12 +49,10 @@ ActiveRecord::Schema.define(version: 20180221224004) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.bigint "cart_id"
     t.bigint "user_id"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -99,9 +89,6 @@ ActiveRecord::Schema.define(version: 20180221224004) do
     t.integer "role", default: 0
   end
 
-  add_foreign_key "carts", "accessories"
-  add_foreign_key "carts", "users"
-  add_foreign_key "orders", "carts"
   add_foreign_key "orders", "users"
   add_foreign_key "trips", "conditions"
 end
