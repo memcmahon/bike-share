0dcb5782166273edb83e16827a185e1c485f8633
Joe Jobes <jmrjobes@gmail.com>
2018-02-22 17:40:27 -0700

    Add show action

--- app/controllers/accessories_controller.rb
+++ app/controllers/accessories_controller.rb
@@ -1,7 +1,17 @@
 class AccessoriesController < ApplicationController
+  before_action :set_accessory, only: [:show]
+
+  def show
+  end
 
   def index
     @accessories = Accessory.all
   end
+
+  private
+
+    def set_accessory
+      @accessory = Accessory.find(params[:id])
+    end
 
 end
--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,5 +1,5 @@
 class Accessory < ApplicationRecord
   validates_presence_of :name, :price, :description, :status
-
+  
   enum status: ["Active", "Retired"]
 end
