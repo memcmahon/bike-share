0925b29855bedeb23f3ec3f9bb855fbc2967f17f
Joe Jobes <jmrjobes@gmail.com>
2018-02-22 20:22:34 -0700

    Add test so that users can not see links for edit delete and create accessory

--- spec/features/admins/admin_can_see_index_of_accessories_spec.rb
+++ spec/features/admins/admin_can_see_index_of_accessories_spec.rb
@@ -18,5 +18,20 @@ describe "as an admin" do
       expect(page).to have_link("Edit")
       expect(page).to have_link("New Accessory")
     end
+    it "doesn't show edit and delete for user" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      accessories = create_list(:accessory, 4)
+
+      visit bike_shop_index_path
+
+      expect(page).to have_link(accessories[0].name)
+      expect(page).to have_link(accessories[1].name)
+      expect(page).to have_link(accessories[2].name)
+      expect(page).to have_link(accessories[3].name)
+      expect(page).to_not have_button("Delete")
+      expect(page).to_not have_link("Edit")
+      expect(page).to_not have_link("New Accessory")
+    end
   end
 end
