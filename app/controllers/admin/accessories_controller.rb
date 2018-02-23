77d01c734fee7e9fdbf006f555ac43d043d04a1c
Joe Jobes <jmrjobes@gmail.com>
2018-02-22 19:53:47 -0700

    Add create action to controller

--- app/controllers/admin/accessories_controller.rb
+++ app/controllers/admin/accessories_controller.rb
@@ -1,5 +1,23 @@
 class Admin::AccessoriesController < Admin::BaseController
 
-  
+  def new
+    @accessory = Accessory.new
+  end
+
+  def create
+    @accessory = Accessory.new(accessory_params)
+    if @accessory.save
+      flash[:success] = "You have successfully created an accessory"
+      redirect bike_shop_path(@accessory)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def accessory_params
+    params.require(:accessory).permit(:price, :name, :description, :role)
+  end
 
 end
