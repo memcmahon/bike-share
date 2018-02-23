b4e673d43880b4aa1b0fdc74ee23d3bd3f47d52e
Joe Jobes <jmrjobes@gmail.com>
2018-02-23 07:40:21 -0700

    Add instance method

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,5 +1,10 @@
 class Accessory < ApplicationRecord
   validates_presence_of :name, :price, :description, :status
-  
+
   enum status: ["Active", "Retired"]
+
+  def convert_to_dollars
+    price.to_s.insert(-3, '.').prepend('$')
+  end
+
 end
