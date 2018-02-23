2dc78dfc51717d8f0b10bb53d1d48125a53dd2d2
Joe Jobes <jmrjobes@gmail.com>
2018-02-22 17:06:09 -0700

    Add admin can create new accessory

---
+++ spec/features/admins/admin_can_create_new_accessory_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "as an admin" do
+  describe "when I am on the new accessory page" do
+    describe "and fill out the form, then hit create" do
+      it "creates a new accessory" do
+        admin = create(:admin)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+        accessories = create_list(:accessory, 4)
+
+        visit bike_shop_path
+        click_on "Create Accessory"
+
+        fill_in 'accessory[price]', with: '$400.00'
+        fill_in 'accessory[name]', with: 'Wild Locally Sourced Organic Buttered Lettuce'
+        fill_in 'accessory[description]', with: 'Found in the tundras of alaska. This lettuce has been naturally buttered by bears and beavers.'
+        click_on "Create Accessory"
+
+        expect(page).to have_link("Wild Locally Sourced Organic Buttered Lettuce")
+        expect(page).to have_link('$400.00')
+      end
+    end
+  end
+end
