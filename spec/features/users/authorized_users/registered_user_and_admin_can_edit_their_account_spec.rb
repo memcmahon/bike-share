75883dfb50904ef93f0c90a8b3b46854193aa38b
Luis Garcia <garcia@yahoo.com>
2018-02-24 16:36:16 -0700

    Add admin edit/modify account test

--- spec/features/users/authorized_users/registered_user_and_admin_can_edit_their_account_spec.rb
+++ spec/features/users/authorized_users/registered_user_and_admin_can_edit_their_account_spec.rb
@@ -25,4 +25,29 @@ describe "As a registered user and admin" do
       expect(page).to have_content("theDB@gmail.com")
     end
   end
+
+  describe "As A regular user" do
+    it "they can edit/modify their accounts" do
+      admin = create(:user, role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit edit_user_path(admin)
+
+      expect(page).to have_content("Modify Account")
+
+      fill_in "user[first_name]", with: "Drew"
+      fill_in "user[last_name]", with: "Briggs"
+      fill_in "user[email]", with: "theDB@gmail.com"
+      fill_in "user[password]", with: "brigsy"
+      fill_in "user[password_confirmation]", with: "brigsy"
+
+      click_on "Submit"
+
+      expect(current_path).to eq admin_dashboard_path
+
+      expect(page).to have_content("Drew")
+      expect(page).to have_content("Briggs")
+      expect(page).to have_content("theDB@gmail.com")
+    end
+  end
 end
