8a8dc40d5a76bcc9ce8801f17137fa64e97c6316
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-23 22:52:39 -0700

    Adds feature spec for user and admin station show page

---
+++ spec/features/stations/user_and_admin_sees_station_show_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe "as a user or admin" do
+  describe "when I visit a station show" do
+    before(:each) do
+      @user = create(:user)
+      @station_1 = create(:station, name: "Ali")
+      @station_2 = create(:station, name: "Ilana")
+      @station_3 = create(:station, name: "Sal")
+      @condition_1 = create(:condition, date: Date.strptime("10/3/2017", "%m/%e/%Y"))
+      @condition_2 = create(:condition, date: Date.strptime("11/3/2016", "%m/%e/%Y"))
+      @trip_1 = Trip.create!(duration: 200, start_date: Date.strptime("10/3/2017", "%m/%e/%Y"), start_station: @station_1, end_date: Date.strptime("10/3/2017", "%m/%e/%Y"), end_station: @station_2, bike_id: 14, subscription_type: "Customer", zip_code: 52556, condition: @condition_1)
+      @trip_2 = Trip.create!(duration: 250, start_date: Date.strptime("10/3/2017", "%m/%e/%Y"), start_station: @station_1, end_date: Date.strptime("10/3/2017", "%m/%e/%Y"), end_station: @station_1, bike_id: 13, subscription_type: "Subscriber", zip_code: 55555, condition: @condition_1)
+      @trip_2 = Trip.create!(duration: 250, start_date: Date.strptime("10/6/2017", "%m/%e/%Y"), start_station: @station_1, end_date: Date.strptime("10/3/2017", "%m/%e/%Y"), end_station: @station_2, bike_id: 13, subscription_type: "Subscriber", zip_code: 52556, condition: @condition_1)
+      @trip_3 = Trip.create!(duration: 300, start_date: Date.strptime("11/3/2016", "%m/%e/%Y"), start_station: @station_2, end_date: Date.strptime("11/3/2016", "%m/%e/%Y"), end_station: @station_3, bike_id: 13, subscription_type: "Subscriber", zip_code: 55555, condition: @condition_2)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    it "I see the number of rides started at this station" do
+      visit station_show(@station_1)
+
+      expect(page).to have_content("Trips started here: 3")
+    end
+
+    it "I see the number of rides ended at this station" do
+      visit station_show(@station_3)
+
+      expect(page).to have_content("Trips ended here: 1")
+    end
+
+    it "I see the most frequent destination station for rides started at this station" do
+      visit station_show(@station_1)
+
+      expect(page).to have_content("Most frequent destination from this stop: #{@station_2}")
+    end
+
+    it "I see the most frequent origination station for rides ended at this station" do
+      visit station_show(@station_2)
+
+      expect(page).to have_content("Most frequent origination to this stop: #{@station_1}")
+    end
+
+    it "I see the date with the highest number of rides started at this station" do
+      visit station_show(@station_1)
+
+      expect(page).to have_content("Date with most rides started: 10/03/2017")
+    end
+
+    it "I see the most frequent zip code for trips starting at this station" do
+      visit station_show(@station_1)
+
+      expect(page).to have_content("Most frequent zip code for trips starting here: 52556")
+    end
+
+    it "I see the most frequent bike_id starting from this station" do
+      visit station_show(@station_1)
+
+      expect(page).to have_content("Most frequent bike id for trips starting here: 13")
+    end
+  end
+end
