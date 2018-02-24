5addccc929f958bcac95e29ba7c9680879802d5d
Joe Jobes <jmrjobes@gmail.com>
2018-02-24 06:58:56 -0700

    Add feature test for cart show path

--- spec/features/cart/visitor_can_add_accessory_to_cart_spec.rb
+++ spec/features/cart/visitor_can_add_accessory_to_cart_spec.rb
@@ -10,7 +10,7 @@ describe "as a user" do
         visit accessories_path
 
         click_on 'Add to Cart'
-        
+
         expect(page).to have_content("You now have 1 Wild Locally Sourced Buttered Lettuce in your cart")
       end
       it "incrememts correctly with consecutive button clicks" do
@@ -23,7 +23,6 @@ describe "as a user" do
         click_on 'Add to Cart'
 
         expect(page).to have_content("You now have 2 Wild Locally Sourced Buttered Lettuces in your cart")
-
       end
     end
   end
---
+++ spec/features/cart/visitor_sees_contents_of_cart_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "as a visitor" do
+  describe "when I navigate to /cart from the cart button" do
+    before(:each) do
+      @accessory = create(:accessory)
+    end
+    it "shows cart contents and count" do
+      
+      visit cart_path
+
+      expect(page).to have_content("Cart Count: 0")
+
+      visit accessories_path
+
+      click_on "Add to Cart"
+
+      visit carts_path
+
+      expect(page).to have_content("Cart Count: 1")
+    end
+  end
+end
