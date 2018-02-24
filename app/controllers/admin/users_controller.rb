d7614e46c92176ebe3c3e0a74f1461cd676b3095
Luis Garcia <garcia@yahoo.com>
2018-02-24 13:35:04 -0700

    Remove edit and update crud out of admin controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,20 +1,3 @@
 class Admin::UsersController < Admin::BaseController
   
-  def edit
-    @user = User.find_by(slug: params[:slug])
-  end
-
-  def update
-    @user = User.find(params[:slug])
-    @user.update(user_params)
-    flash[:notice] = "#{@user.name} was updated"
-
-    redirect_to user_path(@user.slug)
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password)
-    end
 end
