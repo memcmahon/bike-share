1f03e72ecb7fb0fcf82c8544875a90a5f8dc67dc
Joe Jobes <jmrjobes@gmail.com>
2018-02-24 09:58:26 -0700

    Add increase action to controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
   include ActionView::Helpers::TextHelper
 
   def show
-
+    @accessories = @cart.cart_accessories
   end
 
   def create
@@ -13,6 +13,11 @@ class CartsController < ApplicationController
 
     flash[:success] = "You now have #{pluralize(session[:cart][accessory.id.to_s], accessory.name)} in your cart."
     redirect_to accessories_path
+  end
+
+  def increase
+    @cart.add_accessory(params[:accessory_id])
+    redirect_to '/cart'
   end
 
 end
