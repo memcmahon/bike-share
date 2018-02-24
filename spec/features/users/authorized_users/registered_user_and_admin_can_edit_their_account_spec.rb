e3058956ed4b2ca304f9a77cc5e5ca29e7ac25a2
Luis Garcia <garcia@yahoo.com>
2018-02-24 16:12:01 -0700

    Change first test to regular user test

--- spec/features/users/authorized_users/registered_user_and_admin_can_edit_their_account_spec.rb
+++ spec/features/users/authorized_users/registered_user_and_admin_can_edit_their_account_spec.rb
@@ -1,13 +1,7 @@
 require 'rails_helper'
 
 describe "As a registered user and admin" do
-  it "they can edit/modify their accounts" do
-    user = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit edit_user_path(user)
 
-    expect(page).to have_content("Modify Account")
-  save_and_open_page
   end
 end
