850b603b668cfb45151977b4630802545a4ae1e3
Joe Jobes <jmrjobes@gmail.com>
2018-02-25 10:28:42 -0700

    Add more test for instance methods

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -11,4 +11,32 @@ describe Order, type: :model do
     it { should have_many(:order_accessories) }
     it { should have_many(:accessories).through(:order_accessories) }
   end
+
+  describe "Instance Methods" do
+    before(:each) do
+      user = create(:user)
+      @accessories = create_list(:accessory, 2)
+      @order = Order.create(user_id: user.id,
+                            status: 0,
+                            total: 6000)
+      OrderAccessory.create(order_id: @order.id,
+                      accessory_id: @accessories[0].id,
+                      quantity: 2)
+      OrderAccessory.create(order_id: @order.id,
+                      accessory_id: @accessories[1].id,
+                      quantity: 3)
+    end
+    describe "#accessory_quantity" do
+      it "returns quantity of accessory in order" do
+        expect(@order.accessory_quantity(@accessories[0].id)).to eq(2)
+        expect(@order.accessory_quantity(@accessories[1].id)).to eq(3)
+      end
+    end
+    describe "#accessory_subtotal" do
+      it "returns subtotal cost of accessory order" do
+        expect(@order.accessory_subtotal(@accessories[0].id)).to eq(2400)
+        expect(@order.accessory_subtotal(@accessories[1].id)).to eq(3600)
+      end
+    end
+  end
 end
