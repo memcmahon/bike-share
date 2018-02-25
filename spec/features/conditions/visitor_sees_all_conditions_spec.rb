f696041f14800ef19a1d0c012d0d1d7b042822ab
Joe Jobes <jmrjobes@gmail.com>
2018-02-25 15:27:02 -0700

    update spec with new links

--- spec/features/conditions/visitor_sees_all_conditions_spec.rb
+++ spec/features/conditions/visitor_sees_all_conditions_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 describe "visitor sees all conditions" do
-  context "link from the root path" do
+  context "navigate to conditions index" do
     it "shows all conditions" do
       visitor = create(:user)
       condition = create_list(:condition, 2)
 
-      visit root_path
-      click_on "Conditions"
+      visit conditions_path
 
       expect(current_path).to eq(conditions_path)
       expect(page).to have_content(condition[0].month_day_converter)
--- spec/features/users/user_sees_all_conditions_spec.rb
+++ spec/features/users/user_sees_all_conditions_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe "user sees all conditions" do
-  context "link from the root path" do
+  context "navigates to conditions index" do
     it "shows all conditions" do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       condition = create_list(:condition, 2)
 
-      visit root_path
-      click_on "Conditions"
+      visit conditions_path
 
       expect(current_path).to eq(conditions_path)
       expect(page).to have_content(condition[0].month_day_converter)
