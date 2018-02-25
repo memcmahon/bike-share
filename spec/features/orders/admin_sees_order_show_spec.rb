e3f2481313dc1379cf1585e7c4b5cbc9db65ecf1
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-25 12:21:46 -0700

    Adds feature spec for admin order show

---
+++ spec/features/orders/admin_sees_order_show_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe "As an admin" do
+  describe "when they visit an order page" do
+    before(:each) do
+      @admin = create(:admin)
+      @user_1 = create(:user)
+      @accessory_1, @accessory_2, @accessory_3 = create_list(:accessory, 3)
+      @order_1 = @user_1.orders.create!(user: @user_1, status: 0)
+      @line_1 = OrderAccessory.create!(order: @order_1, accessory: @accessory_1, quantity: 1)
+      @line_2 = OrderAccessory.create!(order: @order_1, accessory: @accessory_2, quantity: 2)
+      @line_3 = OrderAccessory.create!(order: @order_1, accessory: @accessory_3, quantity: 3)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:admin)
+      visit admin_order_path(@order)
+    end
+
+    it "they see the oder date and time" do
+      expect(page).to have_content("Order date: #{@order_1.created_at}")
+    end
+
+    it "they see the purchaser's name and address" do
+      expect(page).to have_content("Purchaser Name: #{@user_1.first_name} #{@user_1.last_name}")
+      expect(page).to have_content("Purchaser Address: #{@user_1.address} #{@user_1.city} #{@user_1.state} #{@user_1.zip_code}")
+    end
+
+    it "they see a link to each order item page" do
+      click_on(@accessory_2.name)
+
+      expect(current_path).to eq(accessory_path(@accessory_2))
+    end
+
+    it "they see the quantity and item subtotal" do
+      expect(page).to have_content("#{@accessory_3.name} quantitiy: #{@line_3.quantity} subtotal: #{@order_1.accessory_subtotal(@accessory_3.id)}")
+    end
+
+    it "they see the total for the order" do
+      expect(page).to have_content("Total: #{@order_1.total}")
+    end
+
+    it "they see the status of the order" do
+      expect(page).to have_content("Status: #{@order_1.status}")
+    end
+  end
+end
