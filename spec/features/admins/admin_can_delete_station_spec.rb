5be1c376ee78295cf69e308abe9a48d9336ec9d8
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-25 11:10:00 -0700

    Updates tests for data changes

--- spec/features/admins/admin_can_delete_station_spec.rb
+++ spec/features/admins/admin_can_delete_station_spec.rb
@@ -9,19 +9,18 @@ describe "As an admin" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit station_path(station.slug)
-      
+
       expect(page).to have_content("Station Details")
       expect(page).to have_content("1 Street")
       expect(page).to have_content("City: San Francisco")
-      expect(page).to have_content("1991-12-12")
-      
+      expect(page).to have_content("12/12/1991")
+
       click_on "delete"
 
       expect(page).to have_content("Stations")
       expect(page).to have_content("1 Street was deleted")
       expect(page).to_not have_content("City: San Francisco")
-      expect(page).to_not have_content("1991-12-12")
+      expect(page).to_not have_content("12/12/1991")
     end
   end
 end
- 
