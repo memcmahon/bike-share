03844a01999b87fa1420d104a9e0cb4915e1f125
Joe Jobes <jmrjobes@gmail.com>
2018-02-25 11:06:15 -0700

    Add paperclip validations

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -2,7 +2,9 @@ class Accessory < ApplicationRecord
   validates_presence_of :name, :price, :description, :status
   has_many :order_accessories
   has_many :orders, through: :order_accessories
-  
+  has_attached_file :avatar
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
   enum status: ["Active", "Retired"]
 
   def convert_to_dollars
