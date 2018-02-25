71ea1cf471fab82031fe7e9690bf64b98e6f1feb
Luis Garcia <garcia@yahoo.com>
2018-02-24 18:51:13 -0700

    Create Visitor security test

---
+++ spec/features/users/visitor_security_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "I have limitted access in the app" do
+    it "a visitor cannot visit an admins dashboard" do
+      visit admin_dashboard_path
+
+      expect(page).to have_content("404")
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+
+    it "a visitor cannot visit an registered users dashboard" do
+      visit dashboard_path
+
+      expect(page).to have_content("Log In")
+      expect(page).to have_content("Sign Up")
+    end
+  end
+end
