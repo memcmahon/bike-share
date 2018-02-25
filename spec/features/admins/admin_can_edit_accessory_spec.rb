b8394b1903ab2883dfbe3386b7cf34d7c8e1065f
Joe Jobes <jmrjobes@gmail.com>
2018-02-25 11:30:04 -0700

    Add test for editing accessory

---
+++ spec/features/admins/admin_can_edit_accessory_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "as an admin" do
+  describe "when I am on the bike-shop page and click edit" do
+    describe "and fill the form, then hit update" do
+      it "updates the accessory" do
+        admin = create(:admin)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+        accessory = create(:accessory)
+
+        visit accessories_path
+        click_on "Edit"
+
+        fill_in 'accessory[price]', with: '1200'
+        fill_in 'accessory[name]', with: 'Wild Buttered Lettuce'
+        fill_in 'accessory[description]', with: 'Found in the tundras of alaska. This lettuce has been naturally buttered by bears and beavers.'
+        click_on "Update Accessory"
+
+        expect(page).to have_content("You have successfully updated #{accessory.name}")
+        expect(page).to have_content("Wild Buttered Lettuce")
+        expect(page).to have_content('$12.00')
+      end
+    end
+  end
+end
