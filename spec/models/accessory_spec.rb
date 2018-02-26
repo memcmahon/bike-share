f22d225ec8e1bbcc59bab831b966d5a3a2c1dd98
Luis Garcia <garcia@yahoo.com>
2018-02-26 14:35:46 -0700

    Remove Convert test

--- spec/models/accessory_spec.rb
+++ spec/models/accessory_spec.rb
@@ -18,11 +18,5 @@ describe Accessory, type: :model do
       @accessory_1 = create(:accessory, price: 30000)
       @accessory_2 = create(:accessory, price: 12340)
     end
-    describe "#convert_to_dollars" do
-      it "converts integer to dollar format" do
-        expect(@accessory_1.convert_to_dollars).to eq("$300.00")
-        expect(@accessory_2.convert_to_dollars).to eq("$123.40")
-      end
-    end
   end
 end
