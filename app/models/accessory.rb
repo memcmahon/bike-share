52b0e7161ddbcce1c4b95c1d4a1d8554ef7c1f4b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-26 10:23:15 -0700

    fixes region link

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -4,10 +4,10 @@ class Accessory < ApplicationRecord
   has_many :orders, through: :order_accessories
   has_attached_file :avatar,
                     storage: :s3,
+                    s3_region: 'US East(N.Virgina)',
                     s3_credentials: {access_key_id: ENV["AKIAIKHQYSMPYQKMO4FQ"],
                                      secret_access_key: ENV["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"]},
                     bucket: "bike-share-assets",
-                    region: ENV["US East(N. Virginia)"]
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   enum status: ["Active", "Retired"]
