3a7705db6f9265734bf0039893fa1d83f84af718
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-26 10:34:41 -0700

    Updates name of region

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -4,7 +4,8 @@ class Accessory < ApplicationRecord
   has_many :orders, through: :order_accessories
   has_attached_file :avatar,
                     storage: :s3,
-                    s3_region: 'US East(N.Virgina)',
+                    s3_host_name: "s3-us-east-1.amazonaws.com",
+                    s3_region: 'us-east-1',
                     s3_credentials: {access_key_id: ENV["AKIAIKHQYSMPYQKMO4FQ"],
                                      secret_access_key: ENV["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"]},
                     bucket: "bike-share-assets"
