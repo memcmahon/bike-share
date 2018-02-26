eb11ac02b5d4ffa88dcb5cbd8026ee386ed9426d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-26 12:47:27 -0700

    Updates test for dashboard control

--- app/controllers/conditions_dashboard_controller.rb
+++ app/controllers/conditions_dashboard_controller.rb
@@ -1,7 +1,12 @@
 class ConditionsDashboardController < ApplicationController
 
   def index
-    @conditions = Condition.all
+    if current_user
+      @conditions = Condition.all
+    else
+      flash[:notice] = "Please register or log in to visit this page"
+      redirect_to root_path
+    end
   end
 
 end
--- spec/features/stations/registered_user_sees_stations_dashboard_spec.rb
+++ spec/features/stations/registered_user_sees_stations_dashboard_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe "As a registered user and admin" do
   describe "when I visit the stations-dashboard" do
     it "I see station analytics" do
+      user = create(:user)
       first = create(:station, name: "1st", installation_date: Date.new(1991,12,10))
-      second = create(:station, name: "2st", dock_count: 2) 
+      second = create(:station, name: "2st", dock_count: 2)
       third = create(:station, name: "3st", dock_count: 9)
       fourth = create(:station, name: "4st", installation_date: Date.new(1991,12,16))
-      
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit '/stations-dashboard'
 
       expect(page).to have_content("Total count of stations: 4")
