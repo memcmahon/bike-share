7fc72ff5299811eeb88480d311228d91ef572a3e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-26 17:50:50 -0700

    Adds flash message for non-admins trying to access admin pages

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -5,8 +5,10 @@ private
   def require_admin
     if current_user
       render file: 'public/404' unless current_user.admin?
+      flash[:notice] = "Must be admin to view this page"
     else
       render file: 'public/404'
+      flash[:notice] = "Must be admin to view this page"
     end
   end
 end
