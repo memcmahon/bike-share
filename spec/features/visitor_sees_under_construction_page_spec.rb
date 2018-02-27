94aeb5db6c53a23c51f902e66786dc42512d0a5d
Luis Garcia <garcia@yahoo.com>
2018-02-27 13:39:13 -0700

    replace old epectation with new ones

--- spec/features/visitor_sees_under_construction_page_spec.rb
+++ spec/features/visitor_sees_under_construction_page_spec.rb
@@ -4,9 +4,9 @@ describe "visitor sees under construction page" do
   context "when they visit a root" do
     it "show under construction" do
       visit root_path
-
-      expect(page).to have_content("Sorry")
-      expect(page).to have_content("This Site is Under Construction")
+save_and_open_page
+      expect(page).to have_content("Alchemists Cyclery")
+      expect(page).to have_content("Exploring the Bay Area Bike Share")
     end
   end
 end
