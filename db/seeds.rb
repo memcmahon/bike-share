62d209bb3032e2c310997b407747a575e7325341
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-28 10:54:55 -0700

    final push to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 OPTIONS = {headers: true, header_converters: :symbol }
-# OrderAccessory.destroy_all
-# Station.destroy_all
-# Trip.destroy_all
-# Condition.destroy_all
-# Order.destroy_all
+OrderAccessory.destroy_all
+Station.destroy_all
+Trip.destroy_all
+Condition.destroy_all
+Order.destroy_all
 Accessory.destroy_all
 
 Accessory.create!(name: "Headlight", description: "See things!", price: 1200, status: 0)
@@ -26,45 +26,45 @@ Accessory.create!(name: "Tubes", description: "Air pressure!", price: 1900, stat
 Accessory.create!(name: "Tires", description: "Grip things!", price: 2000, status: 0)
 Accessory.create!(name: "Spokes", description: "Structure!", price: 2100, status: 0)
 Accessory.create!(name: "Hanle wrap", description: "Be unique!", price: 2200, status: 0)
-#
-# CSV.foreach('db/fixtures/weather.csv', OPTIONS) do |row|
-#   Condition.create!(date: Date.strptime(row[:date], '%m/%e/%Y'),
-#                     max_temp_f: row[:max_temperature_f],
-#                     mean_temp_f: row[:mean_temperature_f],
-#                     min_temp_f: row[:min_temperature_f],
-#                     mean_humidity: row[:mean_humidity],
-#                     mean_visibility_miles: row[:mean_visibility_miles],
-#                     mean_wind_speed_mph: row[:mean_wind_speed_mph],
-#                     precipitation_inches: row[:precipitation_inches],
-#                     zip_code: row[:zip_code])
-#   puts "Created #{Condition.last}"
-# end
-#
-# CSV.foreach('db/fixtures/station.csv', OPTIONS) do |row|
-#   Station.create!(id: row[:id],
-#     name: row[:name],
-#     dock_count: row[:dock_count],
-#     city: row[:city],
-#     installation_date: Date.strptime(row[:installation_date], '%m/%e/%Y'))
-#   puts "Created #{Station.last}"
-# end
-#
-# CSV.foreach('db/fixtures/trip.csv', OPTIONS) do |row|
-#   Trip.create!(id: row[:id],
-#                duration: row[:duration],
-#                start_date: Date.strptime(row[:start_date], '%m/%e/%Y'),
-#                start_station: Station.all.shuffle[0],
-#                end_date: Date.strptime(row[:end_date], '%m/%e/%Y'),
-#                end_station: Station.all.shuffle[0],
-#                bike_id: row[:bike_id],
-#                subscription_type: row[:subscription_type],
-#                zip_code: if row[:zip_code] && row[:zip_code].length < 6
-#                             row[:zip_code]
-#                           end,
-#                condition: if Condition.find_by(date: Date.strptime(row[:start_date], '%m/%e/%Y'))
-#                             Condition.find_by(date: Date.strptime(row[:start_date], '%m/%e/%Y'))
-#                           else
-#                             Condition.all.shuffle[0]
-#                           end)
-#   puts "Created #{Trip.last}"
-# end
+
+CSV.foreach('db/fixtures/condition.csv', OPTIONS) do |row|
+  Condition.create!(date: Date.strptime(row[:date], '%m/%e/%Y'),
+                    max_temp_f: row[:max_temperature_f],
+                    mean_temp_f: row[:mean_temperature_f],
+                    min_temp_f: row[:min_temperature_f],
+                    mean_humidity: row[:mean_humidity],
+                    mean_visibility_miles: row[:mean_visibility_miles],
+                    mean_wind_speed_mph: row[:mean_wind_speed_mph],
+                    precipitation_inches: row[:precipitation_inches],
+                    zip_code: row[:zip_code])
+  puts "Created #{Condition.last}"
+end
+
+CSV.foreach('db/fixtures/station.csv', OPTIONS) do |row|
+  Station.create!(id: row[:id],
+    name: row[:name],
+    dock_count: row[:dock_count],
+    city: row[:city],
+    installation_date: Date.strptime(row[:installation_date], '%m/%e/%Y'))
+  puts "Created #{Station.last}"
+end
+
+CSV.foreach('db/fixtures/trip.csv', OPTIONS) do |row|
+  Trip.create!(id: row[:id],
+               duration: row[:duration],
+               start_date: Date.strptime(row[:start_date], '%m/%e/%Y'),
+               start_station: Station.all.shuffle[0],
+               end_date: Date.strptime(row[:end_date], '%m/%e/%Y'),
+               end_station: Station.all.shuffle[0],
+               bike_id: row[:bike_id],
+               subscription_type: row[:subscription_type],
+               zip_code: if row[:zip_code] && row[:zip_code].length < 6
+                            row[:zip_code]
+                          end,
+               condition: if Condition.find_by(date: Date.strptime(row[:start_date], '%m/%e/%Y'))
+                            Condition.find_by(date: Date.strptime(row[:start_date], '%m/%e/%Y'))
+                          else
+                            Condition.all.shuffle[0]
+                          end)
+  puts "Created #{Trip.last}"
+end
